8d330eeb89fd6b921017ce8cef1ed5720b19e720
ym <yangmi0829@vip.qq.com>
2020-05-17 22:58:59 +0800

    测试大数据响应

--- week05/server.js
+++ week05/server.js
@@ -1,11 +1,16 @@
 const http = require('http')
-
+const fs = require('fs')
 http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.headers)
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('X-Foo', 'bar');
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.end('ok');
+    const content = fs.readFileSync('./content.js', {
+        encoding: 'UTF-8'
+    })
+    console.log('返回数据长度:', content.length.toString(16))
+    console.log('返回数据', content)
+    res.end(content);
 }).listen(3000);
